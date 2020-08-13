1f9b085c77e93d20b1df14ab9326c48132736a43
miaopengpeng223 <miaopengpeng223@pingan.com.cn>
2020-08-13 22:58:03 +0800

    css动画和js操作进度条对比

--- src/pages/index/App.vue
+++ src/pages/index/App.vue
@@ -2,60 +2,97 @@
   <div id="app">
     <h1>index page</h1>
     <h2>{{msg}}</h2>
+    <h2>css3动画实现</h2>
     <div id="progressbar">
       <div id="fill" v-text="progressText"></div>
+    </div>
+    <h2>js操作dom实现</h2>
+    <div class="progressBox">
+      <div class="progressBar" v-text="progressText"></div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      msg: "ppm",
-      progressText: "0%",
-    };
-  },
-  created() {
-    let count = 0;
-    const timer = setInterval(() => {
-      count++;
-      this.progressText = count + "%";
-      if (count === 100) {
-        clearInterval(timer);
-      }
-    }, 17);
-  },
-};
+  export default {
+    name: 'app',
+    data() {
+      return {
+        msg: "ppm",
+        progressText: "0%",
+      };
+    },
+    // created() {
+    // },
+    mounted() {
+      // this.$el必须要等到mounted也就是dom元素创建后调用
+      // console.log('el', this.$el.querySelector('#fill').style.setProperty('animation-duration', 2 + 's'));
+      let count = 0;
+      const jsProgressBar = this.$el.querySelector('.progressBar');
+      const timer = setInterval(() => {
+        count++;
+        jsProgressBar.style.width = count + '%';
+        this.progressText = count + "%";
+        if (count === 100) {
+          clearInterval(timer);
+        }
+      }, 17);
+    }
+  };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-#progressbar {
-  width: 180px;
-  height: 20px;
-  margin: 0 auto;
-  // border: 1px solid #000000;
-}
-#fill {
-  animation: move 2s;
-  text-align: center;
-  background-color: rgb(208, 53, 53);
-  color: #ffffff;
-}
-@keyframes move {
-  0% {
-    width: 0;
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
   }
-  100% {
-    width: 100%;
+
+  .progressBox {
+    margin: 1rem auto;
+    width: 18rem;
+    justify-content: center;
+    text-align: center;
+    border: 1px solid #000000;
+    border-radius: .5rem;
   }
-}
+
+  .progressBar {
+    width: 1%;
+    height: 1.4rem;
+    text-align: center;
+    background-color: #1db10a;
+    border-radius: .5rem;
+    color: azure;
+  }
+
+  #progressbar {
+    width: 18rem;
+    margin: 0 auto;
+    border: 1px solid #000000;
+    border-radius: .5rem;
+  }
+
+  #fill {
+    height: 1.4rem;
+    justify-content: center;
+    animation: move 2s;
+    text-align: center;
+    background-color: #1db10a;
+    color: #ffffff;
+    border-radius: .5rem;
+  }
+
+  @keyframes move {
+    0% {
+      width: 0;
+    }
+
+    100% {
+      width: 100%;
+    }
+  }
 </style>
